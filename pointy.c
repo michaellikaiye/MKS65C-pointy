b9223ec862028ef25b06fc20c07385c7739a7aae
Michael Ye <34128025+michaellikaiye@users.noreply.github.com>
2018-09-16 10:16:09 -0400

    Update pointy.c

--- pointy.c
+++ pointy.c
@@ -1,23 +1,3 @@
-/*
-   Declares and initializes an unsigned int to some value > 2.1 billion.
-   Declare a char * that points to the address of your unsigned int.
-
-    Yes, I really do mean char *
-
-   Print out your int in hex.
-
-    The formatting character for a hexadecimal int is: %x
-
-   Use your pointer to print out each individual byte of your int.
-
-    The formatting character for a single byte in hex is %hhx, that is half of half of an integer.
-
-   Using your pointer, modify the individual bytes of your int and print out the resulting value in hex and decimal after each modification.
-
-    Increment each byte by 1
-    Increment each byte by 16
- */
-
 #include <stdio.h>
 unsigned int x = 3216549870;
 char * c = &x;
