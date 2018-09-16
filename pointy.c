fd34513dce2fc4caa0b8842cdf80b1f482c01b7e
Michael Ye <34128025+michaellikaiye@users.noreply.github.com>
2018-09-16 10:14:56 -0400

    done

--- pointy.c
+++ pointy.c
@@ -26,18 +26,18 @@ int main() {
 	printf("byte values of x \n");
   for(i = sizeof(x) - 1 ; i >= 0 ; i--)
 		printf("byte %d: %hhx   ", i, *(c + i));
-	printf("\npointer in hex: %0x \n", c);
+	printf("\npointer in hex: %x \n", c);
 	printf("dec value: %u \nhex value: %x \n", x, x);
 
 	printf("\nbyte values after incrementing by each byte 1\n");
 	for(i = sizeof(x) - 1 ; i >= 0 ; i--) 
 		printf("byte %d: %hhx   ", i, ++*(c + i));
-	printf("\npointer in hex: %0x \n", c);
+	printf("\npointer in hex: %x \n", c);
 	printf("dec value: %u \nhex value: %x \n", x, x);
 
 	printf("\nbyte values after incrementing by each byte 16\n");
 	for(i = sizeof(x) - 1 ; i >= 0 ; i--)
 		printf("byte %d: %hhx   ", i, *(c + i) += 16);
-	printf("\npointer in hex: %0x \n", c);
+	printf("\npointer in hex: %x \n", c);
 	printf("dec value: %u \nhex value: %x \n", x, x);
 }
