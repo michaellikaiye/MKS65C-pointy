647a0e6cd97052ef991679eb9606dc4682d2206b
michaellikaiye <mye1@stuy.edu>
2018-09-17 14:57:32 -0400

    in-class edits

--- pointy.c
+++ pointy.c
@@ -3,21 +3,23 @@ unsigned int x = 3216549870;
 char * c = &x;
 int i;
 int main() {
-	printf("byte values of x \n");
+  printf("byte values of x \n");
   for(i = sizeof(x) - 1 ; i >= 0 ; i--)
-		printf("byte %d: %hhx   ", i, *(c + i));
-	printf("\npointer in hex: %x \n", c);
-	printf("dec value: %u \nhex value: %x \n", x, x);
+    printf("byte %d: %hhx   ", i, *(c + i));
+  printf("\npointer in hex: %x \n", c);
+  printf("dec value: %u \nhex value: %x \n", x, x);
 
-	printf("\nbyte values after incrementing by each byte 1\n");
-	for(i = sizeof(x) - 1 ; i >= 0 ; i--) 
-		printf("byte %d: %hhx   ", i, ++*(c + i));
-	printf("\npointer in hex: %x \n", c);
-	printf("dec value: %u \nhex value: %x \n", x, x);
+  printf("\nbyte values after incrementing by each byte 1\n");
+  for(i = sizeof(x) - 1 ; i >= 0 ; i--) 
+    printf("byte %d: %hhx   ", i, ++*(c + i));
+  printf("\npointer in hex: %x \n", c);
+  printf("dec value: %u \nhex value: %x \n", x, x);
 
-	printf("\nbyte values after incrementing by each byte 16\n");
-	for(i = sizeof(x) - 1 ; i >= 0 ; i--)
-		printf("byte %d: %hhx   ", i, *(c + i) += 16);
-	printf("\npointer in hex: %x \n", c);
-	printf("dec value: %u \nhex value: %x \n", x, x);
+  printf("\nbyte values after incrementing by each byte 16\n");
+  for(i = sizeof(x) - 1 ; i >= 0 ; i--)
+    printf("byte %d: %hhx   ", i, *(c + i) += 16);
+  printf("\npointer in hex: %x \n", c);
+  printf("dec value: %u \nhex value: %x \n", x, x);
+
+  return 0;
 }
